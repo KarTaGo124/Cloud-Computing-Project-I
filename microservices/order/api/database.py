64ad34371f62e03aada63d3b8abdd0ad4774ef19
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-09-29 18:31:09 -0500

    fix: Change DB Host

--- microservices/order/api/database.py
+++ microservices/order/api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:postgres@db:5432/order'
+DATABASE_URL = 'postgresql://postgres:postgres@52.204.117.43:5432/order'
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
