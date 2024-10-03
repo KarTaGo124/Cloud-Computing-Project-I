8bcabdaf297a1681ebba1844dcfdedf0fefdfdac
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-10-03 10:36:24 -0500

    test

--- microservices/user/server/index.ts
+++ microservices/user/server/index.ts
@@ -26,7 +26,9 @@ app.use("/favProducts", favProductRouter);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => {
-	console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const PORT = parseInt(process.env.PORT as string, 10) || 8081;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+	console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
 });
