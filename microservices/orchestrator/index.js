bde83663f2bd3f80d756bf23056f8057390fe59c
flauts <hector.nieto@utec.edu.pe>
2024-10-04 18:45:37 -0500

    a

--- microservices/orchestrator/index.js
+++ microservices/orchestrator/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const axios = require('axios');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocs = require('./swagger.config');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./swagger.config");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: '*',
-  methods: 'GET,POST,PUT,DELETE',
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 /**
  * @swagger
@@ -38,21 +38,29 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
  *       500:
  *         description: Error al obtener el recibo
  */
-app.get('/orchestrator/orders/:orderId/receipt', async (req, res) => {
+app.get("/orchestrator/orders/:orderId/receipt", async (req, res) => {
   const orderId = req.params.orderId;
   try {
-    const orderInfoResponse = await axios.get(`http://api-order:8000/orders/${orderId}`);
-    const orderInfo = orderInfoResponse.data; 
+    const orderInfoResponse = await axios.get(
+      `http://api-order:8000/orders/${orderId}`
+    );
+    const orderInfo = orderInfoResponse.data;
 
-    const orderProductsResponse = await axios.get(`http://api-order:8000/orders/${orderId}/products`);
-    const orderProducts = orderProductsResponse.data; 
+    const orderProductsResponse = await axios.get(
+      `http://api-order:8000/orders/${orderId}/products`
+    );
+    const orderProducts = orderProductsResponse.data;
 
     if (!orderProducts || orderProducts.length === 0) {
-      return res.status(404).json({ message: 'No products found for this order' });
+      return res
+        .status(404)
+        .json({ message: "No products found for this order" });
     }
 
     const productDetailsPromises = orderProducts.map(async (orderProduct) => {
-      const productResponse = await axios.get(`http://product-backend:8080/products/${orderProduct.product_id}`);
+      const productResponse = await axios.get(
+        `http://product-backend:8080/products/${orderProduct.product_id}`
+      );
       const product = productResponse.data;
       return {
         product_id: product.id,
@@ -61,26 +69,33 @@ app.get('/orchestrator/orders/:orderId/receipt', async (req, res) => {
         quantity: orderProduct.quantity,
         imageUrl: product.imageUrl,
         category: product.category,
-        total: product.price * orderProduct.quantity
+        total: product.price * orderProduct.quantity,
       };
     });
 
     const productDetails = await Promise.all(productDetailsPromises);
 
-    const totalPrice = productDetails.reduce((sum, product) => sum + product.total, 0);
+    const totalPrice = productDetails.reduce(
+      (sum, product) => sum + product.total,
+      0
+    );
 
-    const customerId = orderInfo.customer_id;  
-    const userInfoResponse = await axios.get(`http://user-backend:8081/users/${customerId}`);
+    const customerId = orderInfo.customer_id;
+    const userInfoResponse = await axios.get(
+      `http://user-backend:8081/users/${customerId}`
+    );
     const userInfo = userInfoResponse.data;
 
-    const userProfileInfoResponse = await axios.get(`http://user-backend:8081/profile/${customerId}`);
+    const userProfileInfoResponse = await axios.get(
+      `http://user-backend:8081/profile/${customerId}`
+    );
     const userProfileInfo = userProfileInfoResponse.data;
 
     res.json({
       order: {
         id: orderId,
         order_date: orderInfo.order_date,
-        status: orderInfo.status
+        status: orderInfo.status,
       },
       user: {
         id: userInfo.id,
@@ -90,15 +105,16 @@ app.get('/orchestrator/orders/:orderId/receipt', async (req, res) => {
         last_name: userProfileInfo.last_name,
         phone_number: userProfileInfo.phone_number,
         address: userProfileInfo.address,
-        country: userProfileInfo.country
+        country: userProfileInfo.country,
       },
       products: productDetails,
-      totalPrice: totalPrice
+      totalPrice: totalPrice,
     });
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error al obtener el recibo', error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error al obtener el recibo", error: error.message });
   }
 });
 
@@ -124,18 +140,24 @@ app.get('/orchestrator/orders/:orderId/receipt', async (req, res) => {
  *       500:
  *         description: Error al obtener los productos favoritos
  */
-app.get('/orchestrator/users/:userId/favorites', async (req, res) => {
+app.get("/orchestrator/users/:userId/favorites", async (req, res) => {
   const userId = req.params.userId;
   try {
-    const favProductsResponse = await axios.get(`http://user-backend:8081/favProducts/${userId}`);
+    const favProductsResponse = await axios.get(
+      `http://user-backend:8081/favProducts/${userId}`
+    );
     const favProducts = favProductsResponse.data;
 
     if (!favProducts || favProducts.length === 0) {
-      return res.status(404).json({ message: 'No favorite products found for this user' });
+      return res
+        .status(404)
+        .json({ message: "No favorite products found for this user" });
     }
 
     const productDetailsPromises = favProducts.map(async (favProduct) => {
-      const productResponse = await axios.get(`http://product-backend:8080/products/${favProduct.product_id}`);
+      const productResponse = await axios.get(
+        `http://product-backend:8080/products/${favProduct.product_id}`
+      );
       const product = productResponse.data;
       return {
         product_id: product.id,
@@ -143,7 +165,7 @@ app.get('/orchestrator/users/:userId/favorites', async (req, res) => {
         description: product.description,
         price: product.price,
         imageUrl: product.imageUrl,
-        category: product.category
+        category: product.category,
       };
     });
 
@@ -151,15 +173,16 @@ app.get('/orchestrator/users/:userId/favorites', async (req, res) => {
 
     res.json({
       userId: userId,
-      favoriteProducts: productDetails
+      favoriteProducts: productDetails,
     });
-
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error al obtener los productos favoritos', error: error.message });
+    res.status(500).json({
+      message: "Error al obtener los productos favoritos",
+      error: error.message,
+    });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Orchestrator running on http://localhost:${port}`);
